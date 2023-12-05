cc1cfeaf3da96f3149da7f51423a1d58c99b1cc1
Mathéo DELAUNAY <dseonay@gmail.com>
2023-12-05 12:06:02 +0100

    Add SQL queries for film duration, intervener count, and adherent count

--- requetes.sql
+++ requetes.sql
@@ -38,3 +38,68 @@ SELECT *
 FROM INTERVENANTS
 WHERE id_nationalite NOT IN ('FRA', 'USA', 'GBR')
     AND LOWER(nom) LIKE '%r%';
+
+-- 9 Donnez la liste des séances du 20/12/07 comprises entre 18 et 21 heures.
+SELECT *
+FROM SEANCES
+WHERE jour = '2007-12-20'
+    AND heuredeb BETWEEN '18:00:00' AND '21:00:00';
+
+-- 10 Donnez la durée des films en heures et minutes en utilisant des opérations arithmétiques pour réaliser la conversion
+SELECT 
+    titre,
+    CONCAT(
+        FLOOR(duree / 60), 'h ',
+        duree % 60, 'min'
+    ) AS duree_convertie
+FROM FILMS;
+
+-- 11. Donnez les durées minimale, moyenne et maximale des films.
+SELECT 
+    MIN(duree) AS duree_minimale,
+    AVG(duree) AS duree_moyenne,
+    MAX(duree) AS duree_maximale
+FROM FILMS;
+
+-- 12. Donnez le compte du nombre d’intervenants, classé par nationalités différentes.
+
+SELECT id_nationalite, COUNT(*) AS nombre_intervenants
+FROM INTERVENANTS
+GROUP BY id_nationalite;
+
+
+-- 13. Quel est le nombre d’adhérents par ville.
+
+SELECT id_nationalite, COUNT(*) AS nombre_intervenants
+FROM INTERVENANTS
+GROUP BY id_nationalite;
+SELECT ville, COUNT(*) AS nombre_adherents
+FROM ADHERENTS
+GROUP BY ville;
+
+-- 14. Donnez le nombre de femmes parmi les intervenants
+
+SELECT COUNT(*) AS nombre_femmes
+FROM INTERVENANTS
+WHERE sexe = 'F';
+
+-- 15. Donnez de nombre de films correspondant à chaque genre, selon l’ordre décroissant
+
+SELECT nom_genre, COUNT() AS nombre_films
+FROM GENRE
+JOIN FILMS ON GENRE.id_genre = FILMS.id_genre
+GROUP BY nom_genre
+ORDER BY COUNT() DESC;
+
+-- 16 Donnez la liste des comédies françaises.
+SELECT titre
+FROM FILMS
+JOIN SOUS_GENRE ON FILMS.id_sous_genre = SOUS_GENRE.id_sous_genre
+JOIN GENRE ON SOUS_GENRE.id_genre = GENRE.id_genre
+WHERE nom_genre = 'Comédie' AND id_nationalite = 'FRA';
+
+-- 17 Donnez la liste des nationalités des réalisateurs.
+SELECT DISTINCT id_nationalite
+FROM INTERVENANTS
+JOIN FILMS ON INTERVENANTS.id_intervenant = FILMS.id_film;
+
