9bbf52b219c4eafb5423056d03e1b303e27f9679
Mathéo DELAUNAY <dseonay@gmail.com>
2023-12-05 11:18:08 +0100

    Fix sorting issue and add query for comedies under 120 minutes

--- requetes.sql
+++ requetes.sql
@@ -1,10 +1,14 @@
 
--- Sélectionne tous les films et les trie par titre de manière ascendante
+-- 1 Sélectionne tous les films et les trie par titre de manière ascendante
 SELECT *
 FROM FILMS
 ORDER BY titre ASC;
 
--- Donnez la liste des nationalités des intervenants.
+-- 2 Donnez la liste des nationalités des intervenants.
 SELECT id_nationalite
 FROM INTERVENANTS;
 
+-- 3 Quels sont les comédies de moins de 120 mn.
+SELECT titre
+FROM FILMS
+WHERE id_sous_genre = 3 AND duree < 120;
